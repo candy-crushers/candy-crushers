9c1f190eed19ceb22a466957ac2f1a51ce547fae
Mary Warrick <mewarrick@gmail.com>
2018-06-06 11:35:14 -0500

    added test for CandyItem component

---
+++ client/components/candy-item.spec.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+import { assert } from 'chai'
+
+import CandyItem from './CandyItem'
+
+describe('<CandyItem>', () => {
+  it('displays the candy name in an h1', () => {
+    const candyCane = {
+       name : 'Candy Cane',
+       description : 'minty and stripe-y',
+       price : 2.99,
+      images : ["../../puplic/defaultPhotos/lolies.jpg"]
+    };
+    const wrapper = shallow(<CandyItem product={candyCane} />)
+    assert.equal(wrapper.find('h1').text(), 'Candy Cane')
+  })
+})
+
+
+
+
+
