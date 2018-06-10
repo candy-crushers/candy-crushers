632a41f99de7ee8d8f49d4c929d41331a37fc379
Mary Warrick <mewarrick@gmail.com>
2018-06-10 11:05:34 -0500

    Update change-status-form.js

--- client/components/admin/change-status-form.js
+++ client/components/admin/change-status-form.js
@@ -8,15 +8,15 @@ function ChangeStatusForm(props){
     <form name="status" >
     <p>{`Oder Status: ${order.status}`}</p>
       <label htmlFor="status">Update order status</label>
-      <select name="status" onChange={(e)=> handleChange(e)}>
+      <select name="status" onChange={handleChange}>
         <option>***</option>
         <option name="status" value="Created">Created</option>
         <option name="status" value="Processing">Processing</option>
         <option name="status" value="Completed">Completed</option>
         <option name="status" value="Cancelled">Cancelled</option>
       </select>
-      {/* <Button onClick={(e)=> handleDelete(e)}>Delete Order</Button> */}
-      <button type="submit" onClick={(e)=> handleEdit(e)}>Change Status</button>
+      {/* <Button onClick={handleDelete}>Delete Order</Button> */}
+      <button type="submit" onClick={handleEdit}>Change Status</button>
     </form>)
 }
 
