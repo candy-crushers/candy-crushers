1902669eac72da3ca810385002489f20b4f7f9e0
chawes <hawes.conor@gmail.com>
2018-06-12 15:05:41 -0500

    Loading and Order Summary component for confirmation
    connects to #159
    connects to #160

--- client/components/checkout/checkout-summary.js
+++ client/components/checkout/checkout-summary.js
@@ -5,6 +5,7 @@ import { Header } from 'semantic-ui-react'
 const CheckoutSummary = ({ cart, subtotal }) => {
   return (
     <div>
+      <Header color='red' as='h3'>Order Summary</Header>
       <CheckoutProductDetails cart={cart} />
       <div>
         <Header as='h4' textAlign='right'>Total: <DisplayAmount amount={subtotal} /></Header>
