1ad7717d953158d06d668b84383c7f5405492cbb
Mary Warrick <mewarrick@gmail.com>
2018-06-12 21:28:36 -0500

    User order history

--- client/components/order/order-detail.js
+++ client/components/order/order-detail.js
@@ -1,26 +1,78 @@
 import React from 'react'
 import {DisplayAmount} from '../'
+import { Table, Header, Image, Grid, Menu } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
-const OrderDetail = ({ order }) => {
+
+function OrderDetailUser({ order }){
   if (!order.id) return <div />
   const addrLine1Indx = order.shippingAddress.indexOf(',')
+  const address = order.shippingAddress.slice(0, addrLine1Indx) + order.shippingAddress.slice(addrLine1Indx + 1)
   return (
     <div>
-      <div>
-        <h3>Shipping Address</h3>
-        <div>
-          <p>{order.shippingAddress.slice(0, addrLine1Indx)}</p>
-          <p>{order.shippingAddress.slice(addrLine1Indx + 1)}</p>
-        </div>
-      </div>
-      <div>
-        <h3>Order Summary</h3>
-        <p>{order.status}</p>
-        <p>Ordered on {order.orderDate}</p>
-        <p>Subtotal: <DisplayAmount amount={order.subtotal} /></p>
-      </div>
-    </div>
-  )
+      <Menu text fluid vertical>
+          <Menu.Item className='header'>Order Number:</Menu.Item>
+          <Menu.Item>{order.id}</Menu.Item>
+      </Menu>
+      <Grid>
+        <Grid.Row columns={3}>
+          <Grid.Column>
+            <Menu text fluid vertical>
+              <Menu.Item className='header'>Order Date:</Menu.Item>
+              <Menu.Item>{order.orderDate}</Menu.Item>
+            </Menu>
+          </Grid.Column>
+          <Grid.Column>
+            <Menu text fluid vertical>
+              <Menu.Item className='header'>Shipped To:</Menu.Item>
+              <Menu.Item>{address}</Menu.Item>
+            </Menu>
+          </Grid.Column>
+          <Grid.Column>
+            <Menu text fluid vertical floated="right">
+              <Menu.Item className='header'>Oder Total:</Menu.Item>
+              <Menu.Item><DisplayAmount amount={order.subtotal} /></Menu.Item>
+            </Menu>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+      <Table striped>
+        <Table.Header>
+          <Table.Row>
+          <Table.HeaderCell></Table.HeaderCell>
+            <Table.HeaderCell>Name</Table.HeaderCell>
+            <Table.HeaderCell>Quantity</Table.HeaderCell>
+            <Table.HeaderCell >Price</Table.HeaderCell>
+            <Table.HeaderCell >Subtotal</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+        {
+          order.products.map((product) =>{
+            return (<Table.Row key={product.id}>
+            <Table.Cell>
+            <Header as='h4' image>
+            <Image src={product.images[7]} rounded size='mini' />
+            </Header>
+            </Table.Cell>
+              <Table.Cell>{product.name}</Table.Cell>
+              <Table.Cell>{product.order_products.quantity}</Table.Cell>
+              <Table.Cell>{<DisplayAmount amount={product.price} />}</Table.Cell>
+              <Table.Cell>{<DisplayAmount amount={product.order_products.quantity * product.price} />}</Table.Cell>
+            </Table.Row>)}
+          )
+        }
+      </Table.Body>
+      </Table>
+      </div>)
+
+
 }
 
-export default OrderDetail
+
+
+export default OrderDetailUser
+
+
+
+
--- client/components/user/user-order-history.js
+++ client/components/user/user-order-history.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { createGetOrdersForUserThunk } from '../../store';
 import { OrderRow } from '../'
+import OrderDetailUser from '../order/order-detail';
+import { Segment, Container } from 'semantic-ui-react'
 
 class UserOrderHistory extends Component {
 
@@ -12,13 +14,15 @@ class UserOrderHistory extends Component {
   render () {
     const { orders } = this.props
     return (
-      <div className="order-history">
-        <h1>Order History</h1>
-        <div className="order-history-orders">
+      <div>
+        <Container><br /><br />
+        <h1>My Order History</h1><br />
+        <div>
           {
-            orders.length > 0 && orders.map(order => <OrderRow key={order.id} order={order} />)
+            orders.length > 0 && orders.map(order => <div key={order.id}><Segment><OrderDetailUser key={order.id} order={order} /></Segment><br /><br /></div>)
           }
         </div>
+        </Container>
       </div>
     )
   }
