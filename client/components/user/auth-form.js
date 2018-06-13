abc1028d1a62809dcee86d7d57a1fd1e6296b0ef
Samir Thakral <samirthakral@icloud.com>
2018-06-13 01:41:54 -0500

    Password Reset Fixed

--- client/components/user/auth-form.js
+++ client/components/user/auth-form.js
@@ -1,39 +1,78 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
-import {auth} from '../../store'
-import { Form, Button, Icon, Segment, Divider } from 'semantic-ui-react'
+import {auth, changePasswordThunk} from '../../store'
+import { Form, Button, Icon, Segment, Divider, Modal } from 'semantic-ui-react'
 
 /**
  * COMPONENT
  */
-const AuthForm = (props) => {
-  const {name, displayName, handleSubmit, error} = props
-  return (
-    <Segment padded id="auth-form">
-      <Form onSubmit={handleSubmit} name={name}>
-        <Form.Field>
-          <label htmlFor="email"><small>Email</small></label>
-          <input name="email" type="text" required />
-        </Form.Field>
-        <Form.Field>
-          <label htmlFor="password"><small>Password</small></label>
-          <input name="password" type="password" required />
-        </Form.Field>
-        {error && error.response && <div> {error.response.data} </div>}
-        <div className="auth-buttons">
-        <Button primary type="submit">{displayName}</Button>
-          <Divider horizontal>Or</Divider>
-          <a href="/auth/google">
-            <Button color="google plus" type="button" >
-              <Icon name="google" />
-              {displayName} with Google
-            </Button>
-          </a>
-        </div>
-      </Form>
-    </Segment>
-  )
+//const AuthForm = (props) => {
+class AuthForm extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      email: ''
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      email: event.target.value
+    })
+  }
+  updatePassword = async (email) => {
+    await this.props.changePassword({email, password: this._password.value, isVerified: true})
+  }
+
+  render(){
+    const {name, displayName, handleSubmit, error} = this.props
+    return (
+      <Segment padded id="auth-form">
+        <Form onSubmit={handleSubmit} name={name}>
+          <Form.Field>
+            <label htmlFor="email"><small>Email</small></label>
+            <input name="email" type="text" required onChange={this.handleChange} />
+          </Form.Field>
+          <Form.Field>
+            <label htmlFor="password"><small>Password</small></label>
+            <input name="password" type="password" required />
+          </Form.Field>
+          {error && error.response && <div> {error.response.data} </div>}
+          {error && error.response.data==='Password has expired. Reset Password' &&
+
+              <Modal trigger={<Button>Update Password</Button>} >
+                <div id="reset-password">
+                <Modal.Header>Update Password</Modal.Header>
+                <Form>
+                  <br />
+                  <Form.Field>
+                    <label>Email: </label>
+                    <input type='text' placeholder={this.state.email} readOnly/>
+                  </Form.Field>
+                  <Form.Field>
+                    <label>New Password: </label>
+                    <input type="password" ref={input => this._password = input} />
+                  </Form.Field>
+                  <Button onClick={() => this.updatePassword(this.state.email)}>Update</Button>
+                </Form>
+                </div>
+              </Modal>
+          }
+          <div className="auth-buttons">
+          <Button primary type="submit">{displayName}</Button>
+            <Divider horizontal>Or</Divider>
+            <a href="/auth/google">
+              <Button color="google plus" type="button" >
+                <Icon name="google" />
+                {displayName} with Google
+              </Button>
+            </a>
+          </div>
+        </Form>
+      </Segment>
+    )
+  }
 }
 
 /**
@@ -67,7 +106,8 @@ const mapDispatch = (dispatch) => {
       const email = evt.target.email.value
       const password = evt.target.password.value
       dispatch(auth(email, password, formName))
-    }
+    },
+    changePassword: (user) => dispatch(changePasswordThunk(user))
   }
 }
 
